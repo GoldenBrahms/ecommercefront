d36246f750cfba55b3afe5779c27a77d351f6d92
GoldenBrahms <chraibibrahim61@gmail.com>
2020-11-08 06:29:58 +0100

    code update

--- src/user/Signin.js
+++ src/user/Signin.js
@@ -5,8 +5,8 @@ import { signin, authenticate, isAuthenticated } from "../auth";
 
 const Signin = () => {
     const [values, setValues] = useState({
-        email: "chraibibrahim61@gmail.com",
-        password: "24011990",
+        email: "",
+        password: "",
         error: "",
         loading: false,
         redirectToReferrer: false
