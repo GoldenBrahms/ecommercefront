c2ad51f51319da66612bfd35cd5005dba65e9373
GoldenBrahms <chraibibrahim61@gmail.com>
2020-11-21 18:11:44 +0100

    salut

--- src/Remerciement.js
+++ src/Remerciement.js
@@ -1,12 +1,17 @@
 import React from 'react';
+import Header from './core/Header';
+
 
 const Remerciement = () => {
     return (
-        <div style={{width:'100%', height:'77vh'}}>
-            <h1 style={{textAlign:'center'}}>Toutes Nos felicitations</h1>
-            <div style={{width:'100%', height:'100px', backgroundColor:'blueviolet'}}></div>
+        <>
+        <Header/>
+        <div style={{width:'100%', height:'77vh', display:'flex', flexDirection:'column', alignItems:'center', justifyContent:''}}>
+            <h1 style={{textAlign:'center'}}>Confirmation de commande</h1>
+                <p>Nous vous remercions de votre commande. Nous vous tiendrons informé par e-mail lorsque les articles de votre commande auront été expédiés.</p>
             <a href="/">Home</a>
         </div>
+        </>
     )
 }
 
--- src/core/ProductDescription.js
+++ src/core/ProductDescription.js
@@ -6,6 +6,7 @@ import LuneRose from '../images/LuneRose.jpg';
 import LightControl from '../images/lightControl.jpeg';
 import Veilleuse from '../images/veilleuse.jpg';
 import LuneVerte from '../images/luneVerte.jpg'
+import AppSourate from '../images/AppSourate.jpeg'
 import SousNavBar from "./SousNavBar";
 import {isAuthenticated} from '../auth';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
@@ -33,7 +34,7 @@ const ProductDescription = () => {
             <div style={{ marginTop:'00px',padding:'20px', backgroundColor:'white', width:'100%',height:'100%', display:'flex',flexDirection:'column', justifyContent:'center', alignItems:'center'}}>
             <div style={{marginTop:'',backgroundColor:'', display:'flex', flexDirection:'column', justifyContent:'left', alignItems:'center'}}>
             <h4 style={{fontSize:'40px', margin:'0', color:'rgb(79, 79, 79)'}}>Découvrez Qurma </h4>
-            <span style={{height:'6px'}}>Avec son nouveau design en forme de lune <br/>et son enceinte intégrée</span>
+            <span style={{height:'6px'}}>Avec son nouveau design en forme de lune <br/>et son enceinte intégrée, elle est élégante</span>
             </div>
             <img style={{marginTop:'50px',width:'70%', height:'100%' }} src={LuneJaune}/>
             </div>
@@ -46,7 +47,8 @@ const ProductDescription = () => {
         color: 'lightgreen'
       }
     }}  >Découvrez Qurma </h1>
-            <span style={{height:'10px'}}>Avec son nouveau design en forme de lune <br/>et son enceinte intégrée</span>
+            <span style={{height:'10px'}}>Avec son nouveau design en forme de lune <br/>et son enceinte intégrée, elle est élégante.<br/> Elle a un air de Lune, cet astre céleste. En plus, <br/>
+                    elle intègre aussi une LED qui permet de génerer<br/> une couleur de illuminer la piece<br/> dans laquelle elle se trouve</span>
             </div>
             <img style={{width:'30%', height:'100%' }} src={LuneJaune}/>
             </div>
@@ -55,7 +57,7 @@ const ProductDescription = () => {
             <>
              <div style={{padding:'10px' ,width:'100%', backgroundColor:'', textAlign:'center'}}>
                 <div>
-                    <h1 style={{margin:'0'}}>Qurma</h1>
+                    <h1 style={{margin:'0'}}>Cet LED</h1>
                     <p>Une application est fourni avec Qurma pour pouvoir la piloter depuis son smartphone en toute simplicité</p>
                 </div>
                 <img style={{width:'250px'}} src={LightControl}/>
@@ -75,21 +77,18 @@ const ProductDescription = () => {
              <>
              <div style={{padding:'10px' ,width:'100%', backgroundColor:'', textAlign:'center'}}>
                 <div>
-                    <h1 style={{margin:'0'}}>Qurma</h1>
-                    <p>Une application est fourni avec Qurma pour pouvoir la piloter depuis son smartphone en toute simplicité</p>
+                    <h1 style={{margin:'0'}}>Cette LED génère plusieurs couleurs</h1>
+                    <p>Vous pourrez choisir la couleur qui vous convient</p>
                 </div>
-                <img style={{width:'550px'}} src={LightControl}/>
+                <img style={{width:'900px', marginTop:'50px'}} src={Multicolor}/>
              </div>
-             <div style={{padding:'20px', marginTop:'00px',width:'100%', height:'', backgroundColor:'', display:'flex', flexDirection:'column'}}>
-             <div style={{display:'flex', justifyContent:'center', alignItems:'center', flexDirection:'column'}}>
-                 <h4 style={{margin:'0', position:'', color:'rgb(79, 79, 79)', textAlign:'center'}}>Car Oui, Qurma est Multicouleur.</h4>
-                 <p style={{margin:'00px', fontSize:'19px', color:'rgb(79, 79, 79)', textAlign:'center'}}>Grace à sa led lumineuse intégré, choississez la couleur de votre Qurma
-                 que vous souhaiter. </p>
-             <img style={{width:'900px', marginTop:'50px'}} src={Multicolor}/>
+             <div style={{display:'flex', justifyContent:'center', alignItems:'center'}}>
+                 <img style={{width:'600px'}} src={LightControl}/>
+                 <div>
+                 <h4 style={{margin:'0', position:'', color:'rgb(79, 79, 79)', textAlign:'center'}}>Depuis l'application ou depuis la télécommande</h4>
+                 <p style={{margin:'00px', fontSize:'19px', color:'rgb(79, 79, 79)', textAlign:'center'}}>Vous pourrez modifier la couleur en toute simplicité. </p>
+                 </div>
              </div>
- 
- 
-         </div>
          </>
             }
             
@@ -106,11 +105,14 @@ const ProductDescription = () => {
             </div>
             :
             <div style={{textAlign:'left', display:'flex', alignItems:'center', justifyContent:'left', backgroundColor:''}}>
-            <img style={{width:'50%', height:'50%'}} src={Smartphone}/>
-            <div style={{backgroundColor:''}}>
-            <h1 style={{margin:'0', color:'rgb(79, 79, 79)'}}>Une application pour simplifier</h1>
-            <p style={{margin:'0'}}>Une application pour plus de maniabilité ainsi pouvoir choisir la sourate<br></br> que l'on souhaite avec son recitateur preferé</p>
-            </div>
+                <div style={{backgroundColor:''}}>
+                    <h1 style={{margin:'0', color:'rgb(79, 79, 79)'}}>Le meilleur pour la fin...</h1>
+                    <p style={{margin:'0'}}>Qurma possède aussi une memoire intégrée<br/> dans laquelle est stocké le majestueux Coran. <br/>Plus de 20 récitateurs des plus connus dans le monde musulman
+                    <br/>Plus de 15 traductions dont le Francais, l'Anglais, le Turc et bien sure l'Arabe<br/>
+                    Diffusez votre son partout
+                    </p>
+                </div>
+                <img style={{width:'750px'}} src={Veilleuse}/>
             </div>
             }
             {width < breakpoint? 
@@ -120,23 +122,12 @@ const ProductDescription = () => {
             </div>
             :
             <div style={{display:'flex', padding:'50px', width:'100%', height:'100vh'}}>
-            <img style={{width:'50%', height:'100%'}} src={LuneRose}/>
+            <img style={{width:'600px'}} src={AppSourate}/>
             <div>
-                <h1 style={{margin:'0', color:'rgb(79, 79, 79)'}}>Une couleur rayonnante qui vous rassurera la nuit</h1>
+                <h1 style={{margin:'0', color:'rgb(79, 79, 79)'}}>Elle contient aussi des Hadiths</h1>
             </div>
             </div>}
-            {width < breakpoint? 
-            <div style={{display:'flex',flexDirection:'column',alignItems:'center', padding:'20px', width:'100%', height:'100vh'}}>
-                <img style={{width:'200px', height:'200px'}} src={Veilleuse}/>
-                <h4 style={{textAlign:'center',marginRight:'20px',marginLeft:'20px', margin:'0', color:'rgb(79, 79, 79)'}}>Une couleur rayonnante qui vous rassurera la nuit</h4>       
-            </div>
-            :
-            <div style={{display:'flex', padding:'50px', width:'100%', height:'100vh'}}>
-            <div>
-                <h1 style={{margin:'0', color:'rgb(79, 79, 79)'}}>traduction en arabe et francais</h1>
-            </div>
-            <img style={{width:'750px'}} src={Veilleuse}/>
-            </div>}
+            
             {width < breakpoint? 
             <>
             <div id="details" style={{width:'100%', height:'55px'}}></div>
@@ -152,6 +143,8 @@ const ProductDescription = () => {
             </div>
             </>
             :
+            <>
+            <h2>Conquis?</h2>
             <div id="details" style={{width:'80%', margin:'50px', border:'1px solid black', display:'flex',flexDirection:'column', alignItems:"left", padding:'50px'}}>
                 <h1>Details</h1>
                 <p><span style={{height: "10px", width: "10px", backgroundColor: "#007600", borderRadius: "50%", display: "inline-block"}}></span> En stock</p>
@@ -160,8 +153,9 @@ const ProductDescription = () => {
                 <h1 style={{margin:'0'}}>29 €</h1>
                 <p style={{color:'#888888'}}>TVA et frais inclus : env. 4.83 €.</p>
                 <p>Retour gratuit dans les 30 jours suivant la date de livraison.</p>
-                <Link className="btn btn-primary btn-sm" to="/CheckoutDirect">Acheter</Link>
+                <a className="btn btn-primary btn-sm" href="/Checkout">Acheter</a>
             </div>
+            </>
             }
         </div>
     );
